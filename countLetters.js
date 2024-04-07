4cb92de953d94999e50728ded47ed628c0d711ea
SorrenJ <soalexjao@gmail.com>
2024-04-07 02:14:28 -0700

    added file

---
+++ countLetters.js
@@ -0,0 +1,42 @@
+// FUNCTION IMPLEMENTATION
+
+//let result;
+let result_style;
+
+function countLetters(letters) {
+  const letterCounts = {};
+
+  for (const letter of letters) {
+    if (letter !== ' ') { // Check if the letter is not a space
+      if (letterCounts[letter]) {
+        letterCounts[letter] += 1;
+      } else {
+        letterCounts[letter] = 1;
+      }
+    }
+  }
+
+  return letterCounts;
+}
+
+const assertEqual = function(actual, expected) {
+
+  //result = console.assert(actual === expected);
+
+  if (actual !== expected) {
+    result_style = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  } else if (actual === expected) {
+    result_style = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  }
+  return result_style;
+};
+
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+
+console.log(countLetters("lighthouse in the house"));
+
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+
