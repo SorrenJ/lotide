104d44b8f1ee4fd34468c86f312173e8f5e6507e
SorrenJ <soalexjao@gmail.com>
2024-04-09 16:08:57 -0700

    added starter eqObject code

---
+++ assertObjectsEqual.js
@@ -0,0 +1,58 @@
+
+const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+const assertEqual = function(actual, expected) {
+
+    result = console.assert(actual === expected);
+  
+    if (actual !== expected) {
+      result_style = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+    } else if (actual === expected) {
+      result_style = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+    }
+    console.log(result_style); 
+    return result_style;
+  };
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+
+    const obj1Keys = Object.keys(object1);
+    const obj2Keys = Object.keys(object2);
+  
+    // if the 2 objects have the same number of keys
+    if (obj1Keys.length !== obj2Keys.length) {
+      return false;
+    }
+  
+    for (const key of obj1Keys) {  // loop through the array of key values for the first object
+  
+      if (!Array.isArray(object1[key]) && object1[key] !== object2[key]) {
+        return false;
+      } else if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }
+  
+    return true;
+
+};
+
+const shirtObject = { color: "red", size: "medium" };
+Object.keys(shirtObject); // => returns ["color", "size"]
+
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject, anotherShirtObject); // => true
+//We need to use that return value in combination with assertEquals to test if the function is working correctly.
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
